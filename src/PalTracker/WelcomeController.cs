a35fa43051e92e9885982e727e96444533f11db3
anila <anila_pradeep@dell.com>
2019-10-14 11:53:52 +0530

    Simple .NET Core Web API app

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -6,6 +6,6 @@ namespace PalTracker
     public class WelcomeController : ControllerBase
     {
         [HttpGet]
-        public string SayHello() => "hi ";
+        public string SayHello() => "hello";
     }
 }
