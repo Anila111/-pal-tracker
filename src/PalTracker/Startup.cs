912e5f40fad30846177186b412a6581bfc71d62f
anila <anila_pradeep@dell.com>
2019-10-15 09:05:43 +0530

    Simple .NET Core Web API app

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -26,6 +26,13 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton(sp => new WelcomeMessage(Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE configured.")));
+            services.AddSingleton(sp => new CloudFoundryInfo(
+              Configuration.GetValue<string>("PORT", "80"),  
+              Configuration.GetValue<string>("MEMORY_LIMIT", "2GB"),  
+              Configuration.GetValue<string>("CF_INSTANCE_INDEX", "1"),  
+              Configuration.GetValue<string>("CF_INSTANCE_ADDR", "127.0.0.1")
+          ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
