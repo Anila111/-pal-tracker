505f07e80ca3dfdd5bc9b8f5e037433a766c9fa4
anila <anila_pradeep@dell.com>
2019-10-16 15:31:06 +0530

    Implemented Accuators

--- src/PalTracker/TimeEntryController.cs
+++ src/PalTracker/TimeEntryController.cs
@@ -2,19 +2,24 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Collections.Generic;
+using PalTracker;
+
 [Route("/time-entries")]
 public class TimeEntryController : ControllerBase
 {
 
     private ITimeEntryRepository repo { get; set; }
-    public TimeEntryController(ITimeEntryRepository timeRepository)
+    private IOperationCounter<TimeEntry> opCounter { get; set; }
+    public TimeEntryController(ITimeEntryRepository timeRepository, IOperationCounter<TimeEntry> operationCounter)
     {
         repo = timeRepository;
+        opCounter = operationCounter;
     }
 
     [HttpGet("{id}", Name = "GetTimeEntry")]
     public IActionResult  Read(long id)
     {
+        opCounter.Increment(TrackedOperation.Read);
         if (!repo.Contains(id))
             return NotFound();
         else
@@ -24,6 +29,7 @@ public class TimeEntryController : ControllerBase
     [HttpDelete("{id}")]
     public IActionResult Delete(long id)
     {
+        opCounter.Increment(TrackedOperation.Delete);
         if (repo.Contains(id))
         {
             repo.Delete(id);
@@ -36,6 +42,7 @@ public class TimeEntryController : ControllerBase
     [HttpPost]
     public IActionResult Create([FromBody] TimeEntry toCreate)
     {
+        opCounter.Increment(TrackedOperation.Create);
         var response = repo.Create(toCreate);
         return CreatedAtRoute("GetTimeEntry", new {id=response.Id}, response);
     }
@@ -43,6 +50,7 @@ public class TimeEntryController : ControllerBase
     [HttpGet]
     public IActionResult List()
     {
+        opCounter.Increment(TrackedOperation.List);
         var response = repo.List();
         return Ok(response);
     }
@@ -50,6 +58,7 @@ public class TimeEntryController : ControllerBase
     [HttpPut("{id}")]
     public IActionResult Update(long id,[FromBody] TimeEntry toUpdate)
     {
+        opCounter.Increment(TrackedOperation.Update);
         if (!repo.Contains(id))
         {
             return NotFound();
